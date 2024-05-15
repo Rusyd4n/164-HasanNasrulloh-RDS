c08592e358bff7efe0341d8d94f5624641702041
Rusyd4n <jauhar.jundi@gmail.com>
2024-05-15 19:31:43 +0700

    Route Edit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,49 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('https://s.id/hasannasrulloh');
+    return view('welcome');
 });
+
+Route::get("/user/{id}/{nama}", function($id,$nama){
+    return[
+        "id" => $id,
+        "nama" => $nama
+    ];
+});
+
+Route::any("detail", function () {
+  $users   = [
+    [
+    "id" => 1,
+    "name" => "abdul",
+    "age" => 30
+    ],
+
+    [
+        "id" => 2,
+        "name" => "syahrul",
+        "age" => "35"
+    ],
+
+    [
+        "id" => 3,
+        "name" => "usman",
+        "age" => 34
+        ]
+    ];
+
+    $result = [];
+    foreach($users as $users){
+        if($users["id"] == 3){
+            array_push($result, $users);
+        }
+        if ($result == []){
+            return "Data tidak ditemukan";
+        }
+    }
+
+    return $result;
+
+
+});
+
