5385b1d9664dca06ca17c083cc793cffde105c6e
Rusyd4n <jauhar.jundi@gmail.com>
2024-05-20 22:22:44 +0700

    masih layouting

--- resources/views/home.php
+++
@@ -1,3 +0,0 @@
-<x-layout>
-    <h3>Ini adalah Halaman Beranda</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('home'));
+/* Route::get('/', function(){
+    return view('home');
+}); */
+
+
 Route::get('/admin', fn() => view('users.admin'));
 Route::get('/guru', fn() => view('users.guru'));
 Route::get('/siswa', fn() => view('users.siswa'));
 Route::get('/login', fn() => view('auth.login'));
 Route::get('/navbar', fn() => view('components.navbar'));
+
+
+use App\Http\Controllers\HomeController;
+Route::get('/', [HomeController::class,'home']);
